19fd0f58259e6d0d738026dae74568b1618ff2bc
Sule_jr92 <Sule_jr92@STHQ012D-02>
2023-01-22 16:42:08 +0400

    Add project files.

---
+++ TicTacToeClientSide/MainWindow.xaml.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TicTacToeClientSide
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static readonly Socket ClientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private const int port = 27001;
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Connection Event
+
+            ConnectToServer();
+            RequestLoop();
+
+        }
+
+        private void RequestLoop()
+        {
+            var receiver = Task.Run(() =>
+            {
+                while (true)
+                {
+                    ReceiveResponse();
+                }
+            });
+        }
+
+        private void ReceiveResponse()
+        {
+            var buffer = new byte[2048];
+            int received = ClientSocket.Receive(buffer, SocketFlags.None);
+            if (received == 0)
+            {
+                return;
+            }
+            else
+            {
+                var data = new byte[received];
+                Array.Copy(buffer, data, received);
+                string text = Encoding.ASCII.GetString(data);
+                IntegrateToView(text);
+            }
+        }
+
+        private void IntegrateToView(string text)
+        {
+            App.Current.Dispatcher.Invoke((Action)(() =>
+            {
+                var data = text.Split('\n');
+                var row1 = data[0].Split('\t');
+                var row2 = data[1].Split('\t');
+                var row3 = data[2].Split('\t');
+
+                b1.Content = row1[0];
+                b2.Content = row1[1];
+                b3.Content = row1[2];
+
+                b4.Content = row2[0];
+                b5.Content = row2[1];
+                b6.Content = row2[2];
+
+                b7.Content = row3[0];
+                b8.Content = row3[1];
+                b9.Content = row3[2];
+
+            }));
+        }
+
+        private void ConnectToServer()
+        {
+            int attempts = 0;
+            while (!ClientSocket.Connected)
+            {
+                try
+                {
+                    ++attempts;
+                    ClientSocket.Connect(IPAddress.Parse("10.2.13.36"), port);
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+            MessageBox.Show("Connected");
+
+            var buffer = new byte[2048];
+            int received = ClientSocket.Receive(buffer, SocketFlags.None);
+            if (received == 0)
+            {
+                return;
+            }
+            else
+            {
+                var data = new byte[received];
+                Array.Copy(buffer, data, received);
+
+                string text = Encoding.ASCII.GetString(data);
+                this.Title = "Player : " + text;
+                this.Player.Text = this.Title;
+
+
+
+
+
+            }
+
+
+        }
+
+        private void b1_Click(object sender, RoutedEventArgs e)
+        {
+            Task.Run(() =>
+            {
+                App.Current.Dispatcher.Invoke(() =>
+                {
+                    var bt = sender as Button;
+                    string request = bt.Content.ToString() + Player.Text.Split(' ')[2];
+                    SendString(request);
+                });
+            });
+        }
+
+        private void SendString(string request)
+        {
+            byte[] buffer = Encoding.ASCII.GetBytes(request);
+            ClientSocket.Send(buffer, 0, buffer.Length, SocketFlags.None);
+
+        }
+    }
+}
